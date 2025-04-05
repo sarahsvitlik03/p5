e4d61efa1fe7f158bc4bf1b0e166b05f80a940a9
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2025-04-05 11:46:18 -0400

    kid.hpp created

--- P5/kid.hpp
+++ P5/kid.hpp
@@ -4,3 +4,23 @@
 //
 //  Created by Sarah Svitlik on 4/5/25.
 //
+
+#pragma once
+#include "helper.hpp"
+
+class Kid (){
+    
+private:
+    
+    string name;
+    Mood mood;
+    <int> completedJobs;
+    
+public:
+    Kid(string& kidName, Mood kidMood); //reate an empty signalset, add SIGUSR1 and SIGQUIT, and attach it to the thread by calling pthread_sigmask().
+    void print();
+    void moodToday();
+    void selectJob();
+    void run();
+    
+}
